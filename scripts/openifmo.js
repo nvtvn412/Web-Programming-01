e1704a1d6cc69dc72612f99b6ff86bcb15628f9b
vyte <nvt0412@gmail.com>
2019-10-03 23:19:58 +0300

    Fixed 2 methods using window.onload

--- scripts/openifmo.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    let images = document.querySelectorAll("img");
-    images.forEach((img) => {
-        img.onclick = () => {
-            window.open("https://se.ifmo.ru/courses/web");
-        }
-    })
-}
